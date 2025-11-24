c18a3fbc9e4ca40511451ca11ecdc158a6e9a3ca
Quinten Steenhuis <qsteenhuis@gmail.com>
2025-11-24 18:03:10 -0500

    Use new FormFyxer library properly: 1. Pin latest FormFyxer 2. Use background action because LLM renaming takes a lot longer

--- setup.py
+++ setup.py
@@ -54,7 +54,7 @@ setup(name='docassemble.ALDashboard',
       url='https://github.com/SuffolkLITLab/docassemble-ALDashboard',
       packages=find_packages(),
       namespace_packages=['docassemble'],
-      install_requires=['PyGithub>=2.1.1', 'docassemble.ALToolbox>=0.9.2', 'python-docx>=1.1.1', 'openai>=1.0', 'tiktoken', 'pyaml'],
+      install_requires=['PyGithub>=2.1.1', 'docassemble.ALToolbox>=0.9.2', 'python-docx>=1.1.1', 'openai>=1.0', 'tiktoken', 'pyaml', 'formfyxer>=1.0.1'],
       zip_safe=False,
       package_data=find_package_data(where='docassemble/ALDashboard/', package='docassemble.ALDashboard'),
      )
